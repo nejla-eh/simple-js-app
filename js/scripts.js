65f24d63468def369cb01ddecae3027b07e149df
Nejla Efendic Hujdur <nejlaefendic@hotmail.com>
2022-07-26 16:27:08 +0200

    change pokemon type to always be an array

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@ let pokemonList = [
     { name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'] },
     { name: 'Ivysaur', height: 1, type: ['grass', 'poison'] },
     { name: 'Venusaur', height: 2, type: ['grass', 'poison'] },
-    { name: 'Charmander', height: 0.6, type: 'fire' },
-    { name: 'Charmeleon', height: 1.1, type: 'fire' },
+    { name: 'Charmander', height: 0.6, type: ['fire'] },
+    { name: 'Charmeleon', height: 1.1, type: ['fire'] },
     { name: 'Charizard', height: 1.7, type: ['fire', 'flying'] },
-    { name: 'Squirtle', height: 0.5, type: 'water' }
+    { name: 'Squirtle', height: 0.5, type: ['water'] }
 ];
