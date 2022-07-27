e5553e39c1c3c9d7789dea7242e565ef1c566825
Nejla Efendic Hujdur <nejlaefendic@hotmail.com>
2022-07-28 00:42:43 +0200

    add listing of pokemons with new styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-let pokemonList = [
+const pokemonList = [
     { name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'] },
     { name: 'Ivysaur', height: 1, type: ['grass', 'poison'] },
     { name: 'Venusaur', height: 2, type: ['grass', 'poison'] },
@@ -7,3 +7,17 @@ let pokemonList = [
     { name: 'Charizard', height: 1.7, type: ['fire', 'flying'] },
     { name: 'Squirtle', height: 0.5, type: ['water'] }
 ];
+
+document.write('<div class="pokemonList">');
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 1.7) {
+        document.write('<div class="pokemon--big">');
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
+        document.write(' - Wow, that\'s big!');
+    } else {
+        document.write('<div class="pokemon">');
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
+    }
+    document.write('</div>');
+}
+document.write('</div>');
